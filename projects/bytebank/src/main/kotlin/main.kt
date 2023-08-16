d6e5d6f5b80e08230dab696fca9a3d55a84e9bb7
Victor Leme <victorlemefernandes@gmail.com>
2023-08-16 20:48:54 -0300

    Estruturas condicionais

--- projects/bytebank/src/main/kotlin/main.kt
+++ projects/bytebank/src/main/kotlin/main.kt
@@ -9,8 +9,26 @@ fun main() {
     val numeroConta: Int = 1000
     var saldo: Double = 0.0
 
+    saldo += 156.75
+
     println("Titular: $titular")
     println("Idade: $idade")
     println("Nro Conta: $numeroConta")
     println("Saldo: R$$saldo")
+
+    if (saldo > 0.0) {
+        println("Conta positiva!")
+    } else if (saldo == 0.0) {
+        println("Conta neutra.")
+    } else {
+        println("Conta negativa.")
+    }
+
+    when {
+        saldo > 0.0 -> println("Conta positiva!")
+        saldo == 0.0 -> {
+            println("Conta neutra.")
+        }
+        else -> println("Conta negativa.")
+    }
 }

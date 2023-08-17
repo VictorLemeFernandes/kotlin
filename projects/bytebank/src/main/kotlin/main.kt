c967e5eca8c47ff20b042c90624a6f8e37932764
Victor Leme <victorlemefernandes@gmail.com>
2023-08-17 16:48:17 -0300

    Métodos básicos

--- projects/bytebank/src/main/kotlin/main.kt
+++ projects/bytebank/src/main/kotlin/main.kt
@@ -2,19 +2,49 @@ fun main() {
     println("Bem vindo ao Bytebank!\n")
 
     var contaVictor = Conta()
+    var contaCarlos = Conta()
+
     contaVictor.titular = "Victor"
     contaVictor.numero = 100
-    contaVictor.saldo = 1580.00
+    contaVictor.saldo = 0.00
 
     println(contaVictor.titular)
     println(contaVictor.numero)
     println(contaVictor.saldo)
+
+    contaVictor.depositar(150.0)
+
+    println(contaVictor.saldo)
+
+    if (contaVictor.transferir(contaCarlos, 125.0)) {
+        println("Transferência bem sucedida!")
+        println("Saldo atual: R$${contaVictor.saldo}")
+    } else {
+        println("Transferência falhou.")
+    }
 }
 
 class Conta {
     var titular = ""
     var numero = 0
     var saldo = 0.0
+
+    fun depositar(valorDepositado: Double) {
+        saldo += valorDepositado
+    }
+
+    fun sacar(valorSacado: Double) {
+        saldo -= valorSacado
+    }
+
+    fun transferir(contaDestino: Conta, valorTransferido: Double): Boolean {
+        if (valorTransferido <= saldo) {
+            contaDestino.depositar(valorTransferido)
+            saldo -= valorTransferido
+            return true
+        }
+        return false
+    }
 }
 
 fun testaStatusConta(saldo: Double) {
@@ -31,6 +61,7 @@ fun testaStatusConta(saldo: Double) {
         saldo == 0.0 -> {
             println("Conta neutra.")
         }
+
         else -> println("Conta negativa.")
     }
 }
